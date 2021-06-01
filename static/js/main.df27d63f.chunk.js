2c079ba25c0af8e6a335bce1bf23c2457c2dc2f9
Tanujabshelke <tanujabshelke@gmail.com>
2021-06-01 13:44:42 +0530

    Updates

--- static/js/main.df27d63f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-projects"]=this["webpackJsonpmy-projects"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),o=n(3),s=n.n(o),i=(n(8),n(0));var j=function(){return Object(i.jsx)("div",{children:Object(i.jsx)("h1",{children:"Random Qoute Generator"})})};s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(j,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.df27d63f.chunk.js.map
